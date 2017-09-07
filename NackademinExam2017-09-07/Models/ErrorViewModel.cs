d81ad8245db7d95226ac2a558fc8e50524911874
Ester Bergström <ester.d.bergstrom@gmail.com>
2017-09-07 09:53:10 +0200

    Added new MVC project to solution

---
+++ NackademinExam2017-09-07/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NackademinExam2017_09_07.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
