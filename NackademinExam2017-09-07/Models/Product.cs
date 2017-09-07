b8aedc4d04a33c39716e185b3faf23c5864fb74d
Ester Bergström <ester.d.bergstrom@gmail.com>
2017-09-07 10:00:07 +0200

    Added validation to properties in the model Product

--- NackademinExam2017-09-07/Models/Product.cs
+++ NackademinExam2017-09-07/Models/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,7 +9,9 @@ namespace NackademinExam2017_09_07.Models
     public class Product
     {
         public int ProductId { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Range(5, 1000)]
         public decimal Price { get; set; }
     }
 }
